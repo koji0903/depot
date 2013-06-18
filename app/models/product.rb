f0b858bc611ba63464c51b9bc23a06b78c5dec92
Koji HIJIKURO <koji0903@gmail.com>
2013-06-18 13:50:56 +0900

    add LineItem

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 # encoding: utf-8
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+
 
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
@@ -11,4 +15,16 @@ class Product < ActiveRecord::Base
       with: %r{\.(gif|jpg|png)$}i,
       message: 'はGIF、JPG、PNG画像のURLでなければなりません'
   }
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base,'Exist Line Item')
+      return false
+    end
+  end
+
 end
