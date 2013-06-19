38071795ffdd8398df80285060e3574ceaf59708
Koji HIJIKURO <koji0903@gmail.com>
2013-06-19 17:12:45 +0900

    12.1

--- test/functional/orders_controller_test.rb
+++ test/functional/orders_controller_test.rb
@@ -11,9 +11,30 @@ class OrdersControllerTest < ActionController::TestCase
     assert_not_nil assigns(:orders)
   end
 
+  test "requires item in cart" do
+    get :new
+    assert_redirected_to store_path
+    assert_equal 'Cart is empty', flash[:notice]
+  end
+
   test "should get new" do
+    item = LineItem.new
+    item.build_cart
+    item.product = products(:ruby)
+    item.save!
+    session[:cart_id] = item.cart.id
+
     get :new
     assert_response :success
+
+=begin
+    cart = Cart.create
+    LineItem.create(cart: cart, product: products(:ruby))
+    
+    get :new, {}, {cart_id: cart.id}
+    assert_response :success
+=end
+
   end
 
   test "should create order" do
@@ -21,7 +42,7 @@ class OrdersControllerTest < ActionController::TestCase
       post :create, order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type }
     end
 
-    assert_redirected_to order_path(assigns(:order))
+    assert_redirected_to store_path
   end
 
   test "should show order" do
